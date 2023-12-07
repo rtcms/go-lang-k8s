88b56bd069d56e2b874d51c20e71986efde55e3c
RT <35173068+rtcms@users.noreply.github.com>
2023-12-07 18:16:17 -0500

    Minor fixes to client.sh

--- client-src/client.sh
+++ client-src/client.sh
@@ -1,8 +1,17 @@
-#!/bin/bash
+#!/bin/sh
+
+counter=0
 
 while true; do
+    # Increment the counter
+    counter=$((counter+1))
+
+    # Display a message for the curl request
+    echo "Curl request is being sent to http://go-web-service.default.svc.cluster.local, Request no $counter"
+
     # Perform curl requests in parallel and run them in the background
     curl -s "http://go-web-service.default.svc.cluster.local" &
+
     # Wait for a 2-second interval before starting the next iteration
     sleep 2
 
